310d849816d0160fcb39c1e084c52d52f59646aa
ustsl <w@imvo.site>
2025-02-11 17:53:15 +0300

    upd

--- src/components/shared/QuickSpeakComponent/ui/QuickSpeakComponent.tsx
+++ src/components/shared/QuickSpeakComponent/ui/QuickSpeakComponent.tsx
@@ -3,7 +3,7 @@ import styles from './quickSpeakComponent.module.css';
 export const QuickSpeakComponent = ({ quickId }: { quickId: string }) => {
     return (
         <div className={styles.quick}>
-            <iframe src={`https://qsbot.app/${quickId}`} ></iframe>
+            <iframe src={`https://app.qspk.me/${quickId}`} ></iframe>
         </div >
     )
 }
