c590b65f2dd32a6e57a5d389d453747df7a84c88
Viacheslav Ustinov <viacheslav.ustinov.dev@gmail.com>
2025-12-07 22:46:48 +0300

    upd

--- src/proxy.ts
+++ src/proxy.ts
@@ -24,7 +24,7 @@ function getLocale(request: NextRequest): string | undefined {
   }
 }
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
